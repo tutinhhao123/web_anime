c1e4eba2ca5fccbd928938863789cc73a30ad27c
tutinhhao123 <tutinhhao@gmail.com>
2024-06-24 08:57:26 +0700

    success first step

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {StrictMode} from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ✅ Now importing from react-dom/client
+import {createRoot} from 'react-dom/client';
+
+import App from './App';
+
+// 👇️ IMPORTANT: make sure to specify correct ID
+// must be the ID of the div element in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = ({ mode } = { mode: "production" }) => {
+    console.log(`mode is: ${mode}`);
+
+    return {
+            mode,
+            entry: "./src/index.js",
+            devServer: {
+                hot: true,
+                open: true
+            },
+            output: {
+                publicPath: "/",
+                path: path.resolve(__dirname, "build"),
+                filename: "bundle.js"
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.(js|jsx)$/,
+                        exclude: /node_modules/,
+                        loader: "babel-loader"
+                    }
+                ]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: "./public/index.html"
+                }),
+                new webpack.HotModuleReplacementPlugin()
+            ]
+        }
+
+};
